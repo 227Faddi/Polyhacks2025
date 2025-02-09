84bbd8c4459510d2ec7cac8fee230542ff0235be
227Faddi <falukhouma27@gmail.com>
2025-02-09 10:42:26 -0500

    feat: add create your planet page, folder structure, fix styles

--- frontend/src/pages/Planet.jsx
+++ frontend/src/pages/Planet.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import Globe from "react-globe.gl";
 import { Link, useParams } from "react-router-dom";
-import AIMessage from "./AIMessage";
-import { fetchPlanetData } from '../functions';
+import { fetchPlanetData } from "../api";
+import AIMessage from "../components/AIMessage";
 
 const Planet = () => {
   const param = useParams();
@@ -19,13 +19,13 @@ const Planet = () => {
       const data = await fetchPlanetData(planetCurr);
       if (data.status == "success") {
         setPlanetData(data.data);
-        console.log(data.data)
+        console.log(data.data);
       } else {
-        console.log("Failed")
+        console.log("Failed");
       }
-      };
-      fetchPlanet();
-  }, [planetCurr])
+    };
+    fetchPlanet();
+  }, [planetCurr]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -41,7 +41,9 @@ const Planet = () => {
   return (
     <div className="h-screen relative">
       <div className="absolute top-5 left-1/2 transform -translate-x-1/2 z-50 px-4 py-2 rounded-lg shadow-sm text-white border border-white">
-        <h1 className="text-5xl font-bold text-white">Planet Explorer</h1>
+        <Link to="/" className="text-5xl font-bold text-white">
+          Orbitron
+        </Link>
         <p className="text-center text-3xl uppercase mt-4">{planet}</p>
       </div>
       <div className="text-white border border-white absolute left-0 top-1/2 transform -translate-y-1/2 z-50 rounded-md p-6">
@@ -50,22 +52,22 @@ const Planet = () => {
           <ul className="flex flex-col space-y-6">
             <li className="text-2xl" to="/earth"></li>
             <li className="text-2xl" to="/venus">
-              Mass: {planetData?.mass?.massValue ?? ''}
+              Mass: {planetData?.mass?.massValue ?? ""}
             </li>
             <li className="text-2xl" to="/uranus">
-              Volume: {planetData?.vol?.volValue ?? ''}
+              Volume: {planetData?.vol?.volValue ?? ""}
             </li>
             <li className="text-2xl" to="/saturn">
-              Density: {planetData?.density ?? ''}
+              Density: {planetData?.density ?? ""}
             </li>
             <li className="text-2xl" to="/neptune">
-              Gravity: {planetData?.gravity ?? ''}
+              Gravity: {planetData?.gravity ?? ""}
             </li>
             <li className="text-2xl" to="/jupiter">
-              Temperature: {planetData?.avgTemp ?? ''}
+              Temperature: {planetData?.avgTemp ?? ""}
             </li>
             <li className="text-2xl" to="/jupiter">
-              Inclination: {planetData?.inclination ?? ''}
+              Inclination: {planetData?.inclination ?? ""}
             </li>
           </ul>
         </div>
@@ -74,35 +76,69 @@ const Planet = () => {
         <div className="flex flex-col items-center space-y-4">
           <h1 className="text-3xl font-bold">Planets</h1>
           <div className="flex flex-col space-y-6 items-center">
-            <Link className="text-2xl" to="/mercury" onClick={() => setPlanetCurr("mercury")}>
+            <Link
+              className="text-2xl"
+              to="/mercury"
+              onClick={() => setPlanetCurr("mercury")}
+            >
               Mercury
             </Link>
-            <Link className="text-2xl" to="/venus" onClick={() => setPlanetCurr("venus")}>
+            <Link
+              className="text-2xl"
+              to="/venus"
+              onClick={() => setPlanetCurr("venus")}
+            >
               Venus
             </Link>
-            <Link className="text-2xl" to="/earth" onClick={() => setPlanetCurr("earth")}>
+            <Link
+              className="text-2xl"
+              to="/earth"
+              onClick={() => setPlanetCurr("earth")}
+            >
               Earth
             </Link>
-            <Link className="text-2xl" to="/moon" onClick={() => setPlanetCurr("moon")}>
+            <Link
+              className="text-2xl"
+              to="/moon"
+              onClick={() => setPlanetCurr("moon")}
+            >
               Moon
             </Link>
-            <Link className="text-2xl" to="/mars" onClick={() => setPlanetCurr("mars")}>
+            <Link
+              className="text-2xl"
+              to="/mars"
+              onClick={() => setPlanetCurr("mars")}
+            >
               Mars
             </Link>
-            <Link className="text-2xl" to="/jupiter" onClick={() => setPlanetCurr("jupiter")}>
+            <Link
+              className="text-2xl"
+              to="/jupiter"
+              onClick={() => setPlanetCurr("jupiter")}
+            >
               Jupiter
             </Link>
-            <Link className="text-2xl" to="/uranus" onClick={() => setPlanetCurr("uranus")}>
+            <Link
+              className="text-2xl"
+              to="/uranus"
+              onClick={() => setPlanetCurr("uranus")}
+            >
               Uranus
             </Link>
-            <Link className="text-2xl" to="/neptune" onClick={() => setPlanetCurr("neptune")}>
+            <Link
+              className="text-2xl"
+              to="/neptune"
+              onClick={() => setPlanetCurr("neptune")}
+            >
               Neptune
             </Link>
           </div>
         </div>
       </div>
       <div className=" text-white border border-white absolute left-1/2 transform -translate-x-1/2 bottom-0 z-50 rounded-t-md p-8">
-        <h2 className="text-center text-2xl mb-4">Want to know more? Ask here!</h2>
+        <h2 className="text-center text-2xl mb-4">
+          Want to know more? Ask here!
+        </h2>
         <div className="flex">
           <input
             type="text"
